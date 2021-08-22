b33c40c11a9b504803baf2883e104842ddc29443
Paul Thompson <paul.t.thompson83@gmail.com>
2021-08-21 22:58:26 -0500

    Added HelloWorld class.

--- src/org/launchcode/java/demos/lsn1datatypes/Message.java
+++ src/org/launchcode/java/demos/lsn1datatypes/Message.java
@@ -1,15 +1,20 @@
 package org.launchcode.java.demos.lsn1datatypes;
 
-public class Message {
+public abstract class Message {
 
     public static String getMessage(String lang) {
+
 
         if (lang.equals("sp")) {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("ch")) {
+            return "Nǐ hǎo, shìjiè!";
         } else {
             return "Hello, World!";
         }
     }
 }
+
+
