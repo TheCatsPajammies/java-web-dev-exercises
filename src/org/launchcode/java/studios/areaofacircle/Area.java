5b439165ed4a628510b02ccec46ead229d336932
Paul Thompson <paul.t.thompson83@gmail.com>
2021-08-25 22:13:51 -0500

    Completed Area Class with Bonus Missions.

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,33 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.*;
+
+public class Area {
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        double radius = 0;
+        while (radius <= 0) {
+            System.out.print("Please enter a positive, non-zero radius value:");
+
+            String str = input.nextLine();
+
+            try {
+                radius = Double.valueOf(str);
+                // Converts the input value to a double.
+            } catch (NumberFormatException e) {
+                // If the input value can't be converted to a double, the value of radius is reset to 0, an
+                // error message is displayed and the while loop is started over.
+                System.out.println("Radius must be a number value - no words/letters allowed!");
+                radius = 0;
+            }
+        }
+
+        double areaOfCircle = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is: " + areaOfCircle);
+
+    }
+
+}
+
+
